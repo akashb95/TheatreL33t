ac9d7df92d52685e45bd762b353bad09d6b36de2
Akash B <akashb50@yahoo.co.uk>
2018-11-01 14:19:06 +0000

    webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+
+const config = {
+    entry: __dirname + '/static/index.js',
+    output: {
+        path: __dirname + '/static',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+        })
+    ]
+};
+
+module.exports = config;
